f8beb13e4b481871b3efe4afb56b705647836d1f
김형민 <ken1336@naver.com>
2020-04-12 23:44:42 +0900

    q3

--- q3.cpp
+++ q3.cpp
@@ -1,78 +1,97 @@
-#include<iostream>
-#include<vector>
-#include<memory>
+#include <iostream>
+#include <vector>
+#include <memory>
 
+class SubDirectory;
 
-class subDirectory;
+class Directory : public std::enable_shared_from_this<Directory>
+{
 
-class Directory{
-
-    public:
-    
-    Directory(std::string name) : dirName(name){}
-
-    void showSubDirectory(Directory dir){
-        std::cout<<"this directory name: "<<dir.dirName<<std::endl;
-        
-            for(auto &i : children){
-                std::cout<<"/"<<i.dirName<<std::endl;
-                if(!i.children.empty())
-                showSubDirectory(i);
-            }
-        
-        
-    }
-
-    void addSubDirectory(SubDirectory& sub){
-        sub.setParent(std::make_shared<Directory>(this));
-        children.push_back(sub);
-    }
-
-
-    protected:
+public:
+    Directory(std::string name) : dirName(name) {}
+    void showSubDirectory();
+    void addSubDirectory(std::shared_ptr<SubDirectory> sub);
     std::string dirName;
-    std::vector<Directory> children;
+protected:
+    
+    std::vector<std::shared_ptr<SubDirectory>> children;
 };
 
 
-class SubDirectory : public Directory{
-    public:
-    SubDirectory(std::string name) : Directory(name){
-        
-    }
-    //SubDirectory()
+
+class SubDirectory : public Directory
+{
+public:
+    SubDirectory(std::string name) : Directory(name){}
     void setParent(std::shared_ptr<Directory> parent){
         this->parent = parent;
     }
-    private:
+    std::shared_ptr<Directory> getParent(){
+        return this->parent;
+    }
+    void showPath(){
+        auto pp = this->parent;
+        while(!pp){
+            std::cout<<pp->dirName;
+            pp = pp->parent;
+        }
+    }
+private:
     std::shared_ptr<Directory> parent;
-    
 };
 
 
 
- void cmd_mkdir(){
-
-    } 
-    void cmd_cp(){
-
-    }
-    void cmd_rm(){
-
-    }
-int main(){
-
+void Directory::addSubDirectory(std::shared_ptr<SubDirectory> sub)
+{
     
-    Directory dir("/");
-
-    subDirectory dir1("a");
-    subDirectory dir2("b");
-    subDirectory dir3("c");
+    auto pp=std::shared_ptr(shared_from_this());
+    sub->setParent(pp);
+    children.push_back(sub);
     
-    dir.addSubDirectory(dir1);
-    dir.addSubDirectory(dir2);
-    dir1.addSubDirectory(dir3);
-    dir.showSubDirectory(dir);
+
+}
+
+void Directory::showSubDirectory()
+    {
+        
+        std::cout<<"/"<<this->dirName<<std::endl;
+        for (auto &i : children)
+        {
+            std::cout<<"/"<<i->dirName<<std::endl;
+            if (!i->children.empty()){
+                
+                i->showSubDirectory();
+            }
+                
+        }
+    }
+
+void cmd_mkdir()
+{
+
+}
+void cmd_cp()
+{
+}
+void cmd_rm()
+{
+}
+int main()
+{
+
+    auto dir = std::make_shared<Directory>("/");
+
+    auto dir1 = std::make_shared<SubDirectory>("a");
+    auto dir2 = std::make_shared<SubDirectory>("b");
+    auto dir3 = std::make_shared<SubDirectory>("c");
+
+    dir->addSubDirectory(dir1);
+    dir->addSubDirectory(dir2);
+    dir2->addSubDirectory(dir3);
+     
+    dir3->showPath();
+    //dir->showSubDirectory();
 
     return 0;
 }
